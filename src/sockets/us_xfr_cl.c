dda1413838f92035a8d642baae07a87799f5a9fa
ysouyno <ysouyno@163.com>
2019-11-29 14:38:22 +0800

    add listing code in chapter 57

---
+++ src/sockets/us_xfr_cl.c
@@ -0,0 +1,39 @@
+#include "us_xfr.h"
+
+int main(int argc, char *argv[])
+{
+  struct sockaddr_un addr;
+  int sfd;
+  ssize_t num_read;
+  char buf[BUF_SIZE];
+
+  sfd = socket(AF_UNIX, SOCK_STREAM, 0);
+  if (sfd == -1) {
+    errExit("socket");
+  }
+
+  // Construct server address, and make the connection
+
+  memset(&addr, 0, sizeof(struct sockaddr_un));
+  addr.sun_family = AF_UNIX;
+  strncpy(addr.sun_path, SV_SOCK_PATH, sizeof(addr.sun_path) - 1);
+
+  if (connect(sfd, (struct sockaddr_un *)&addr,
+              sizeof(struct sockaddr_un)) == -1) {
+    errExit("connect");
+  }
+
+  // Copy stdin to socket
+
+  while ((num_read = read(STDIN_FILENO, buf, BUF_SIZE)) > 0) {
+    if (write(sfd, buf, num_read) != num_read) {
+      fatal("partial/failed write");
+    }
+  }
+
+  if (num_read == -1) {
+    errExit("read");
+  }
+
+  exit(EXIT_SUCCESS);
+}
---
+++ src/sockets/us_xfr_sv.c
@@ -0,0 +1,59 @@
+#include "us_xfr.h"
+
+#define BACKLOG 5
+
+int main(int argc, char *argv[])
+{
+  struct sockaddr_un addr;
+  int sfd, cfd;
+  ssize_t num_read;
+  char buf[BUF_SIZE];
+
+  sfd = socket(AF_UNIX, SOCK_STREAM, 0);
+  if (sfd == -1) {
+    errExit("socket");
+  }
+
+  // Construct server socket address, bind socket to it,
+  // and make this a listening socket
+
+  if (remove(SV_SOCK_PATH) == -1 && errno != ENOENT) {
+    errExit("remove-%s", SV_SOCK_PATH);
+  }
+
+  memset(&addr, 0, sizeof(struct sockaddr_un));
+  addr.sun_family = AF_UNIX;
+  strncpy(addr.sun_path, SV_SOCK_PATH, sizeof(struct sockaddr_un) - 1);
+
+  if (bind(sfd, (struct sockaddr_un *)&addr,
+           sizeof(struct sockaddr_un)) == -1) {
+    errExit("bind");
+  }
+
+  if (listen(sfd, BACKLOG) == -1) {
+    errExit("listen");
+  }
+
+  for (; ; ) {
+    cfd = accept(sfd, NULL, NULL);
+    if (cfd == -1) {
+      errExit("accept");
+    }
+
+    // Transfer data from connected socket to stdout until EOF
+
+    while ((num_read = read(cfd, buf, BUF_SIZE)) > 0) {
+      if (write(STDOUT_FILENO, buf, num_read) != num_read) {
+        fatal("partial/failed write");
+      }
+    }
+
+    if (num_read == -1) {
+      errExit("read");
+    }
+
+    if (close(cfd) == -1) {
+      errMsg("close");
+    }
+  }
+}
